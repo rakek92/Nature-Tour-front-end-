4b7d4f64697bdd66f3ebd33091ea754afc00bcc8
MishevIhor <vbitd92@gmail.com male = vbitd92@gmail.com>
2017-03-07 14:58:54 +0200

    test work

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var slideIndex =1;
+showSlides(slideIndex);
+
+function currentSlide(n){
+	showSlides(slideIndex=n);
+}
+
+
+function showSlides(n){
+	var i;
+	var slides = document.getElementsByClassName("Myslides")
+	var dots = document.getElementsByClassName("dot");
+	if (n>slides.length){
+		slideIndex=1;
+	}
+	if (n<1){
+		slideIndex=slides.length;
+	}
+	for (i=0;i<slides.length;i++){
+		slides[i].style.display="none";
+	}
+	for(i=0;i<dots.length;i++){
+		dots[i].className=dots[i].className.replace("active","")
+	}
+	slides[slideIndex-1].style.display="block";
+	dots[slideIndex-1].className+=" active";
+		}
+
+function slideTime(n){
+ n=1
+ showSlides(slideIndex += n);
+ }
+setInterval(slideTime, 5000);﻿
